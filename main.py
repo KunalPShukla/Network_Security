2e643adab25820e9a5802ef8e3f610c7c2a5439c
KunalPShukla <kpshukla3@gmail.com>
2025-04-11 20:28:19 +0530

    The first commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from Network_security.components.data_ingestion import DataIngestion
+from Network_security.components.data_validation import DataValidation
+from Network_security.components.data_transformation import DataTransformation
 from Network_security.exception.exception import NetworkSecurityException
 from Network_security.logging.logger import logging
-from Network_security.entity.config_entity import DataIngestionConfig
+from Network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from Network_security.entity.config_entity import TrainingPipelineConfig
+
+ 
+
 import sys
-
-
 
 if __name__=='__main__':
     try:
@@ -14,7 +17,22 @@ if __name__=='__main__':
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(dataingestionartifact)
 
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info("Initiate the data Tranformation")
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("data Transformation Completed")
+        print(data_transformation_artifact)
+    
     except Exception as e:
-           raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e,sys)
